d7cebea263c5d986aa070c041ed364ceb348c615
Tatiana <gazaryan.ta@gmail.com>
2021-02-13 17:29:26 -0800

    Дополнительные исправления

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const getRandomNumber = function(min, max) {
   const random = min + Math.random() * (max + 1 - min);
-  if (min < 0 && max < 0) {
+  if (min < 0 || max < 0 || max <=min) {
     throw('Диапазон может быть только положительным');
   }
   return Math.floor(random);
@@ -9,7 +9,7 @@ getRandomNumber(0, 10);
 
 const getRandomFloat = function(min, max, decimal = 2) {
   const random = min + Math.random() * (max + 1 - min);
-  if (min < 0 && max < 0 && max <= min) {
+  if (min < 0 || max < 0 || max <= min) {
     throw('Диапазон может быть только положительным');
   }
   return random.toFixed(decimal);
