02962404097cd48aff75150c1fa6e689dc134bb1
Tatiana <gazaryan.ta@gmail.com>
2021-02-28 16:34:23 +0300

    Генерирование массива

--- js/main.js
+++ js/main.js
@@ -15,3 +15,90 @@ const getRandomFloat = function(min, max, decimal = 2) {
   return random.toFixed(decimal);
 }
 getRandomFloat(0, 10, 2);
+
+const TITLE = 'Уютное жилье для вашего отпуска';
+const DESCRIPTION = 'Оставайтесь у нас';
+const LOCATION = {
+  x: getRandomFloat(35.65000, 35.7000, 2),
+  y: getRandomFloat(139.70000, 139.80000, 2),
+}
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const RANDOM_OBJ_COUNT = 10;
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomNumber(0, elements.length - 1)];
+};
+
+const createRandomArray = (arrays) => {
+  return arrays.slice(getRandomNumber(0, arrays.length-1));
+};
+
+const createAuthor = () => {
+  return {
+    avatar: `img/avatars/user0${getRandomNumber(1,8)}.png`,
+  }
+}
+
+const createOffer = () => {
+  return {
+    title: TITLE,
+    address: LOCATION,
+    price: getRandomNumber(1, 10000),
+    type: getRandomArrayElement(TYPE),
+    room: getRandomNumber(1,5),
+    guests: getRandomNumber(1,5),
+    checkin: getRandomArrayElement(CHECKIN),
+    checkout: getRandomArrayElement(CHECKOUT),
+    features: createRandomArray(FEATURES),
+    description: DESCRIPTION,
+    photos: createRandomArray(PHOTOS),
+  }
+}
+
+const createLocation = () => {
+  return {
+    location: LOCATION,
+  }
+}
+
+
+const createObj = () => {
+  return {
+    author: createAuthor(),
+    offer: createOffer(),
+    location: createLocation(),
+  }
+}
+
+new Array(RANDOM_OBJ_COUNT).fill(null).map(() => createObj());
